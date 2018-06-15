f959082da376ebf625e8c18fa39dde446a8398c7
DanBee <miliam5234@gmail.com>
2018-06-15 23:49:42 +0900

    detailMountain View done

---
+++ Sherpa/Viewcontroller/DetailMountainViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailMountainViewController.swift
+//  Sherpa
+//
+//  Created by DanBee on 2018. 6. 15..
+//  Copyright © 2018년 신동규. All rights reserved.
+//
+
+import UIKit
+
+class DetailMountainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
