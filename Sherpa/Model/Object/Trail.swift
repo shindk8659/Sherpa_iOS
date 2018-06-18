4beb392691df08deaa448c7274a005c8c341f180
Minseob Yoon <yoon.minseob@navercorp.com>
2018-06-19 01:00:51 +0900

    모델변경, 네트워크 사용 변경

--- Sherpa/Model/Object/Trail.swift
+++ Sherpa/Model/Object/Trail.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+struct TrailAttribute {
+    var pathName: String?
+    var difficulty: String?
+    var ascendTime: Int?
+    var descendTime: Int?
+    var distance: Double?
+    
+    init(pathName: String?, difficulty: String?, ascendTime: Int?, descendTime: Int?, distance: Double?) {
+        self.pathName = pathName
+        self.difficulty = difficulty
+        self.ascendTime = ascendTime
+        self.descendTime = descendTime
+        self.distance = distance
+    }
+}
+
+struct Trail {
+    var attributes: TrailAttribute?
+    var paths: [[Double]]?
+}
--- Sherpa/ViewController/Detail/List/DetailListViewController+Network.swift
+++ Sherpa/ViewController/Detail/List/DetailListViewController+Network.swift
@@ -12,28 +12,15 @@ import SwiftyJSON
 extension DetailListViewController {
     
     func requestRecommendedMountains(pageNo: Int, completion: @escaping ([Mountain]?) -> Void) {
-        guard let endPoint = recommend?.endPoint else {
-            return
-        }
-        let url = "http://52.78.104.4:8080\(endPoint)"
         var parameters = recommend?.parameters ?? [:]
         parameters["pageNo"] = pageNo
-        Alamofire.request(url, method: .get, parameters: parameters, headers: nil).responseJSON { response in
-            switch response.result {
-            case .success:
-                if let data = response.data {
-                    do {
-                        let jsonString = JSON(data)["meta"].description
-                        let jsonData = jsonString.data(using: .utf8) ?? Data()
-                        let mountains = try JSONDecoder().decode([Mountain].self, from: jsonData)
-                        completion(mountains)
-                    } catch let error {
-                        print(error)
-                    }
-                }
-            case .failure:
-                break
+        let router = APIRouter(url: recommend?.endPoint ?? "", method: .get, parameters: parameters)
+        NetworkRequestor(with: router).request { (mountains: [Mountain]?, error) in
+            guard error == nil else {
+                completion(nil)
+                return
             }
+            completion(mountains)
         }
     }
 }
