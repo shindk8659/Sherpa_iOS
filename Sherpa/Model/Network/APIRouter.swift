4b04e98e2d3917106fe787c15c5287ff0dbf3ac0
Minseob Yoon <yoon.minseob@navercorp.com>
2018-06-19 01:00:20 +0900

    네트워크 리퀘스트 변경

--- Sherpa/Model/Network/APIRouter.swift
+++ Sherpa/Model/Network/APIRouter.swift
@@ -6,4 +6,28 @@
 //  Copyright © 2018년 신동규. All rights reserved.
 //
 
-import Foundation
+import Alamofire
+
+struct APIConfiguration {
+    static let baseURL = "http://52.78.104.4:8080"
+}
+
+struct APIRouter {
+    var url: String
+    var method: HTTPMethod
+    var parameters: Parameters?
+    var headers: HTTPHeaders?
+    
+    init(url: String, method: HTTPMethod, parameters: Parameters? = nil, headers: HTTPHeaders? = nil) {
+        self.url = url
+        self.method = method
+        self.parameters = parameters
+        self.headers = headers
+    }
+}
+
+extension APIRouter {
+    var requestUrl: String {
+        return APIConfiguration.baseURL + url
+    }
+}
