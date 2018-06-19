e041cc938addbde86240bdefeb8fd4d346a0793e
DanBee <miliam5234@gmail.com>
2018-06-19 11:38:35 +0900

    add Article cell

---
+++ Sherpa/ViewController/MainTableView/MainBoxCell/ArticleCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleCollectionViewCell.swift
+//  Sherpa
+//
+//  Created by DanBee on 2018. 6. 18..
+//  Copyright © 2018년 신동규. All rights reserved.
+//
+
+import UIKit
+
+class ArticleCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var timeLabel: UILabel!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var bodyLabel: UILabel!
+    
+}
